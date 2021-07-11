5ce1ac6456ba5097f64ec002bc2cc4aa13c41f6a
lincmba <lincolncmba@gmail.com>
2021-07-11 17:57:25 +0300

    CSV Upload:  - Form to facilitate file upload  - Custom csv upload method that creates products from csv file  - Update changelist page to have csv_upload link

--- acme/products/admin.py
+++ acme/products/admin.py
@@ -1,13 +1,49 @@
+import unicodecsv as ucsv
+from django import forms
 
 from django.contrib import admin
 
-
+from django.shortcuts import render, redirect
+from django.urls import path
 from acme.products.models import Product
 
-# Register your models here.
 
+
+class CsvImportForm(forms.Form):
+    csv_file = forms.FileField()
 
 
 class ProductAdmin(admin.ModelAdmin):
-    pass
+
+    change_list_template = "products/product_changelist.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('import-csv/', self.import_csv),
+        ]
+        return my_urls + urls
+
+    def import_csv(self, request):
+        """
+        Function to import csv
+        Uses a form to upload the csv, and performs a product creation or
+        update based on whether the sku exists in the database
+        """
+        if request.method == "POST":
+            csv_file = request.FILES["csv_file"]
+            csv_reader = ucsv.DictReader(csv_file, encoding='utf-8-sig')
+            for row in csv_reader:
+                Product.objects.update_or_create(
+                        sku=row.get('sku'), name=row.get('name'),
+                        description=row.get('description'))
+            self.message_user(request, "Your csv file has been imported")
+            return redirect("..")
+        form = CsvImportForm()
+        payload = {"form": form}
+        return render(
+            request, "products/csv_form.html", payload
+        )
+
+
 admin.site.register(Product, ProductAdmin)
