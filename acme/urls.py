e692fa6047a8095700c3ae6c899e8ecdccd8a41f
lincmba <lincolncmba@gmail.com>
2021-07-12 01:59:18 +0300

    Redirect root to admin

--- acme/urls.py
+++ acme/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, reverse_lazy
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url=reverse_lazy('admin:index'))),
 ]
 admin.site.site_header = "ACME"
 admin.site.site_title = "Acme Inc"
