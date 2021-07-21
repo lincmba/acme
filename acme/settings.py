5de723083fce1c2bcc3c8dcb0eee71aa15c578e8
lincmba <lincolncmba@gmail.com>
2021-07-21 12:19:45 +0300

    Move secrets to heroku config
    fixes #3

--- acme/settings.py
+++ acme/settings.py
@@ -23,16 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dfay+!n1!x@hdi02&=)c)j2^m1apa7s*3p-w5ag))cwc0-i!i!'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    "https://acme-inc-products.herokuapp.com",
-    "localhost",
-    "127.0.0.1"
-]
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
 
 
 # Application definition
